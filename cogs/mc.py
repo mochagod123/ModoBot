294bcb24a4e068cd4bfcd3d451a078ba8e320ab2
mochagod123 <yuuta4083@gmail.com>
2024-08-04 09:20:59 +0900

    ok

--- cogs/mc.py
+++ cogs/mc.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import sys
+import math
 
 class Minecraft(commands.Cog):
     def __init__(self, bot):
@@ -14,6 +15,25 @@ class Minecraft(commands.Cog):
         await ctx.message.delete()
         await asyncio.sleep(5)
         await msg.delete()
+
+    @mc.group()
+    @commands.has_permissions()
+    @commands.cooldown(1, 10, type=commands.BucketType.user)
+    async def gnnid(self, ctx, name: str):
+        hexm=name.encode('utf-16be', 'replace').hex().upper()
+        offset=0x00
+        code='30000000 109CED18\n10000000 50000000\n31000000 00000050'
+        hexc=len(hexm)
+        if(len(name)%2)!=0:
+            hexc=len(hexm)+8
+        for x in range(math.floor(hexc/8)):
+            code=code+'\n0012'+format(offset+x*4,'04X')+' '+hexm[x*8:x*8+4]
+            code=code+hexm[x*8+4:x*8+8]
+        if(len(name)%2)!=0:
+            code=code+'0000'
+        code=code+'\n0012'+format(offset+x*4+4,'04X')+' 00000000'+'\nD0000000 DEADCAFE'
+        embed=discord.Embed(title=f"NNID: {name}", description=f"{code}", color=0x006eff)
+        await ctx.send(embed=embed)
 
     @mc.group()
     @commands.has_permissions()
